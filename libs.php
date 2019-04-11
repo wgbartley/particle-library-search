aa03cc4aea3418952cdcab39c9eda872aa7d987e
Garrett Bartley <wgbartley@gmail.com>
2019-04-11 15:03:17 +0000

    Removed output so it can be run from command line without spamming the terminal

--- libs.php
+++ libs.php
@@ -8,6 +8,7 @@ $last_ts = filemtime($file);
 if($last_ts<time()-$cache_time)
 	file_put_contents($file, file_get_contents($url));
 
-header('Content-type: application/json');
-echo file_get_contents($file);
+// No more output since this script should be run manually now instead
+// header('Content-type: application/json');
+// echo file_get_contents($file);
 ?>
